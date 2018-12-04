4a95a1d2df545a4d1d5d2503b3ffe368d4653f31
Anish-Agnihotri <contact@anishagnihotri.com>
2018-12-03 19:25:25 -0500

    Add images + func.

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,7 +4,7 @@ $(function () {
 
     $.getJSON('https://api.myjson.com/bins/16wav2', function (data) {
         $.each(data, function (key, val) {
-            var kudo = "<div class='kudo-card all " + val.quality + "'><div></div><div><h3>" + val.name + "</h3><p>" + val.description + "</p><a class='readMore' href='" + val.link + "'>Read More</></div><div><a href='" + val.link + "'>Print</a></div></div>"
+            var kudo = "<div class='kudo-card all " + val.quality + "'><div><div><img src='" + val.imagesrc + "'/></div></div><div><h3>" + val.name + "</h3><p>" + val.description + "</p><a class='readMore' href='" + val.link + "'>Read More</></div><div><a href='" + val.link + "'>Print</a></div></div>"
             $(kudo).appendTo(".appendCards");
         });
 
