8bde73255effacb93bb7482614f2945e70e636cb
Anish-Agnihotri <contact@anishagnihotri.com>
2018-12-16 15:12:52 -0500

    Adding preview and fixes.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -5,7 +5,7 @@ $(function () {
     $.getJSON('https://api.myjson.com/bins/fyzvw', function (data) {
         $.each(data, function (key, val) {
             var kudo = "<div class='card all " + val.quality + "'><a class='myBtn' href='#" + val.idnumber + "' rel='modal:open'><div class='cardBack'></div><div class='cardFront'><img src='" + val.imagesrc + "'/></div><div><h3>" + val.name + "</h3></div></a></div>"
-            var modal = "<div id='" + val.idnumber + "' class='modal'><div><p>Your selected kudo:</p><div><div><img src='" + val.imagesrc + "' /></div><div><h3>" + val.name + "</h3><p>" + val.description + "</p></div></div></div><div><p>Generate your custom KudoKard:</p><input type='text' name='qr' placeholder='Airdrop Link' disabled /><input type='text' name='message' placeholder='What would you like to say?' /><input type='text' name='from' placeholder='Who is it from?' /></div><div><p>Preview KudoKard:</p></div><div><button>Download PDF</button></div></div>"
+            var modal = "<div id='" + val.idnumber + "' class='modal'><div><p>Your selected kudo:</p><div><div><img src='" + val.imagesrc + "' /></div><div><h3>" + val.name + "</h3><p>" + val.description + "</p></div></div></div><div><p>Generate your custom KudoKard:</p><input type='text' name='qr' placeholder='Airdrop Link (coming soon)' disabled /><input id='message' type='text' name='message' placeholder='What would you like the message to be?' /><input id='sender' type='text' name='from' placeholder='Who is it from?' /></div><div><a class='Button download'>Download PDF</a><a class='Button preview' href='#preview' rel='modal:open'>Preview</a></div></div>"
             $(kudo).appendTo("#cardFocus");
             $(modal).appendTo("#popupModals");
         });
@@ -23,3 +23,14 @@ var $btns = $('.btn').click(function () {
     $btns.removeClass('active');
     $(this).addClass('active');
 })
+
+$('#message').on('change', function() {
+    var text = "<p>something</p>"
+    $(text).appendTo("#field");
+    document.getElementById('previewFrame').src += '';
+});
+
+$('#sender').on('change', function() {
+    $('#senderField').text( this.value );
+    document.getElementById('previewFrame').src += '';
+});
