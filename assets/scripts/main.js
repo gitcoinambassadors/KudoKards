8a40a70840c422c6e30c93057a64b73fff793bb1
Anish-Agnihotri <contact@anishagnihotri.com>
2018-12-16 12:43:28 -0500

    Adding popup modals.

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -5,7 +5,7 @@ $(function () {
     $.getJSON('https://api.myjson.com/bins/fyzvw', function (data) {
         $.each(data, function (key, val) {
             var kudo = "<div class='card all " + val.quality + "'><a class='myBtn' href='#" + val.idnumber + "' rel='modal:open'><div class='cardBack'></div><div class='cardFront'><img src='" + val.imagesrc + "'/></div><div><h3>" + val.name + "</h3></div></a></div>"
-            var modal = "<div id='" + val.idnumber + "' class='modal'>" + val.name + "</div>"
+            var modal = "<div id='" + val.idnumber + "' class='modal'><div><p>Your selected kudo:</p><div><div><img src='" + val.imagesrc + "' /></div><div><h3>" + val.name + "</h3><p>" + val.description + "</p></div></div></div><div><p>Generate your custom KudoKard:</p><input type='text' name='qr' placeholder='Airdrop Link' disabled /><input type='text' name='message' placeholder='What would you like to say?' /><input type='text' name='from' placeholder='Who is it from?' /></div><div><p>Preview KudoKard:</p></div><div><button>Download PDF</button></div></div>"
             $(kudo).appendTo("#cardFocus");
             $(modal).appendTo("#popupModals");
         });
