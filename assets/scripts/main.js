b72de8469bf6429f5a237e33e332e5cdee1f5c69
anish-agnihotri <contact@anishagnihotri.com>
2018-12-24 18:16:29 -0500

    Removing miscommit ajax

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,31 +24,3 @@ var $btns = $('.btn').click(function () {
     $btns.removeClass('active');
     $(this).addClass('active');
 })
-
-$.ajax({
-    type: “POST”,
-    url: “https://mandrillapp.com/api/1.0/messages/send.json”,
-    data: {
-      ‘key’: ‘YOUR API KEY HERE’,
-      ‘message’: {
-        ‘from_email’: ‘YOUR@EMAIL.HERE’,
-        ‘to’: [
-            {
-              ‘email’: ‘RECIPIENT_NO_1@EMAIL.HERE’,
-              ‘name’: ‘RECIPIENT NAME (OPTIONAL)’,
-              ‘type’: ‘to’
-            },
-            {
-              ‘email’: ‘RECIPIENT_NO_2@EMAIL.HERE’,
-              ‘name’: ‘ANOTHER RECIPIENT NAME (OPTIONAL)’,
-              ‘type’: ‘to’
-            }
-          ],
-        ‘autotext’: ‘true’,
-        ‘subject’: ‘YOUR SUBJECT HERE!’,
-        ‘html’: ‘YOUR EMAIL CONTENT HERE! YOU CAN USE HTML!’
-      }
-    }
-   }).done(function(response) {
-     console.log(response); // if you're into that sorta thing
-   });
