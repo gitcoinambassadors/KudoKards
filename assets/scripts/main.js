158932231b2094dc2371265798bf9198856e67eb
anish-agnihotri <contact@anishagnihotri.com>
2018-12-24 16:45:04 -0500

    Changing from printing to digital

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -24,26 +24,3 @@ var $btns = $('.btn').click(function () {
     $btns.removeClass('active');
     $(this).addClass('active');
 })
-
-$('#messager').on('change', function() {
-    $('#messageField').text(this.value);
-});
-
-$('#sender').on('change', function() {
-    $('#senderField').text( this.value );
-});
-
-
-
-// TODO: Rewrite this to process printing func.
-$("#thisButton").click(function () {
-    var divContents = $("#printablearea").html();
-    var printWindow = window.open('', '', 'height=400,width=800');
-    printWindow.document.write('<html><head><title>DIV Contents</title>');
-    printWindow.document.write('</head><body >');
-    printWindow.document.write(divContents);
-    printWindow.document.write('</body></html>');
-    printWindow.document.close();
-    printWindow.print();
-});
-
