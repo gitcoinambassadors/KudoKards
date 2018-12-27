502c1fe72a063027f3fa78d65a782a013bbdf0ba
anish-agnihotri <contact@anishagnihotri.com>
2018-12-27 12:48:43 -0500

    Integrate EmailJS

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -6,7 +6,7 @@ $(document).ready(function () {
         $.getJSON('https://api.myjson.com/bins/fyzvw', function (data) {
             $.each(data, function (key, val) {
                 var kudo = "<div class='card all " + val.quality + "'><a class='myBtn' href='#" + val.idnumber + "' rel='modal:open'><div class='cardBack'></div><div class='cardFront'><img src='" + val.imagesrc + "'/></div><div><h3>" + val.name + "</h3></div></a></div>"
-                var modal = "<div id='" + val.idnumber + "' class='modal'><div><p>Your selected kudo:</p><div><div><img class='kudoImage' src='" + val.imagesrc + "' /></div><div><h3>" + val.name + "</h3><p>" + val.description + "</p></div></div></div><div><p>Generate your custom Gitcoin card:</p><form><input class='recipient' type='email' name='email' placeholder='What is the recipients email?' /><input class='message' type='text' name='message' placeholder='What is your message?' /><input class='sender' type='text' name='from' placeholder='What is your name?' /><input class='redeem' type='text' name='redeem' placeholder='Airdrop link (copy and paste directly).' /><input class='submit' type='submit' value='Send Card' /><input class='preview' type='button' value='Preview' /></form></div></div>"
+                var modal = "<div id='" + val.idnumber + "' class='modal'><div><p>Your selected kudo:</p><div><div><img class='kudoImage' src='" + val.imagesrc + "' /></div><div><h3>" + val.name + "</h3><p>" + val.description + "</p></div></div></div><div><p>Generate your custom Gitcoin card:</p><form class='form'><input class='hiddenLink' type='text' name='hidden_link' /><input class='sender' type='text' name='sender_name' placeholder='What is your name?' /><input class='message' type='text' name='message' placeholder='What is your message?' /><input class='recipientName' type='text' name='to_name' placeholder='What is the recipients name?'/><input class='recipient' type='email' name='reciepient_email' placeholder='What is the recipients email?' /><input class='redeem' type='text' name='link_send' placeholder='Do you have an Airdrop link? (paste directly)' /><input class='submit' type='submit' value='Send Card' /><input class='preview' type='button' value='Preview' /></form></div></div>"
                 $(kudo).appendTo("#cardFocus");
                 $(modal).appendTo("#popupModals");
 
@@ -39,7 +39,7 @@ $(document).ready(function () {
     var myform = $(".form");
     myform.submit(function (event) {
         // Hardcorde link, change when website live.
-        var previewLink2 = 'https://gitcoinambassadors.github.io/KudoKards/preview.html?' + 'MSG=' + $children('.message').val() + 'FROM=' + $children('.sender').val() + 'IMAGEURL=' + $(this).parent().parent().find('.kudoImage').attr('src') + 'CLAIMURL=' + $children('.redeem').val();
+        var previewLink2 = 'https://gitcoinambassadors.github.io/KudoKards/preview.html?MSG=' + encodeURIComponent($(this).children('.message').val().trim()) + 'FROM=' + encodeURIComponent($(this).children('.sender').val().trim()) + 'IMAGEURL=' + $(this).parent().parent().find('.kudoImage').attr('src') + 'CLAIMURL=' + $(this).children('.redeem').val();
         $('.hiddenLink').val(previewLink2);
 
         event.preventDefault();
